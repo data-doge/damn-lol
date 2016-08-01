759ce34cb74ae95c1bf57b7eaf2b9f738fd897c0
data-doge <eugene@enspiral.com>
2016-08-01 16:38:13 -0400

    genesis

---
+++ README.md
@@ -0,0 +1,5 @@
+# damn-lol
+
+ur computer got somethin to tell u
+
+![damn lol](http://i.imgur.com/G4BpJYG.png)
---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var notifier = require('node-notifier')
+var cron = require('node-cron')
+
+notify()
+cron.schedule('*/10 * * * * *', notify)
+
+function notify () {
+  notifier.notify({
+    title: 'DAMN LOL',
+    message: 'DAMN LOL',
+    icon: './ikea.png'
+  })
+}
